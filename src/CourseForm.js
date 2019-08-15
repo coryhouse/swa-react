2438db2595dd81bf08080d25cc5385d7a9b7e89f
Cory House <housecor@gmail.com>
2019-08-15 15:49:27 -0500

    Add course form

--- src/CourseForm.js
+++ src/CourseForm.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CourseForm(props) {
+  const [course, setCourse] = useState({
+    title: "",
+    category: ""
+  });
+
+  function onChange(event) {
+    const newCourse = { ...course }; //copy the course object
+    newCourse[event.target.name] = event.target.value; // the value that someone just typed
+    setCourse(newCourse);
+  }
+
   return (
-    <form>
+    <form onSubmit={() => props.onSubmit(course)}>
       <div>
         <label htmlFor="title">Title</label>
         <br />
-        <input type="text" id="title" />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={onChange}
+          value={course.title}
+        />
       </div>
 
       <div>
         <label htmlFor="category">Category</label>
         <br />
-        <input type="text" id="category" />
+        <input
+          type="text"
+          id="category"
+          onChange={onChange}
+          name="category"
+          value={course.category}
+        />
       </div>
 
       <input type="submit" value="Add Course" />
